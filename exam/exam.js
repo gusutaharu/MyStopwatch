54047fe951913711ca13fa0afb1c228cf19859b1
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-11-13 08:37:13 +0900

    lesson02

--- exam/exam.js
+++ exam/exam.js
@@ -1,5 +1,8 @@
-'use strict'
+'use strict';
 
 {
-  
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
 }
